bbe559e5ec7d0afcc56815453fb66529353ebc95
Christopher Parish <christopher.parish@parishcomputers.com>
2018-04-08 21:31:32 -0400

    Planning some changes for revB

--- fpga/top.v
+++ fpga/top.v
@@ -1,5 +1,16 @@
 module top(
+    input CLK25MHZ,
+    inout[7:0] G,
+    output GPIO_RW
     );
 
+    assign GPIO_RW = 0;
+    
+    reg[28:0] counter;
+    always@(posedge CLK25MHZ) begin
+        counter = counter + 1;
+    end
 
+    assign G[7:0] = counter[28:21];
+    
 endmodule
