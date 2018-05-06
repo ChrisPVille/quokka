797ddf83e02dd5bfff855434143497c390457543
Christopher Parish <christopher.parish@parishcomputers.com>
2018-05-06 00:47:59 -0400

    Added simple UI control

--- fpga/uiControl.v
+++ fpga/uiControl.v
@@ -18,11 +18,39 @@ module uiControl(
     input b_d,
     input b_e,
     input b_f,
-    output[23:0] disp,
+    output reg[23:0] disp,
     output dispValid
     );
     
     assign dispValid = 1;
+    
+    wire somethingPressed;
+    assign somethingPressed = b_0 | b_1 | b_2 | b_3 | b_4 | b_5 | b_6 | b_7 |
+                              b_8 | b_9 | b_a | b_b | b_c | b_d | b_e | b_f;
+    
+    always@(posedge clk or negedge rst_n) begin
+        if(~rst_n) begin
+            disp <= 0;
+        end else if(somethingPressed) begin
+            disp <= {disp[19:4], 4'h0};
+            if(b_0) disp[3:0] <= 4'h0;
+            else if(b_1) disp[3:0] <= 4'h1;
+            else if(b_2) disp[3:0] <= 4'h2;
+            else if(b_3) disp[3:0] <= 4'h3;
+            else if(b_4) disp[3:0] <= 4'h4;
+            else if(b_5) disp[3:0] <= 4'h5;
+            else if(b_6) disp[3:0] <= 4'h6;
+            else if(b_7) disp[3:0] <= 4'h7;
+            else if(b_8) disp[3:0] <= 4'h8;
+            else if(b_9) disp[3:0] <= 4'h9;
+            else if(b_a) disp[3:0] <= 4'ha;
+            else if(b_b) disp[3:0] <= 4'hb;
+            else if(b_c) disp[3:0] <= 4'hc;
+            else if(b_d) disp[3:0] <= 4'hd;
+            else if(b_e) disp[3:0] <= 4'he;
+            else if(b_f) disp[3:0] <= 4'hf;
+        end
+    end
 
 
 endmodule
